cccbe54f2f5dc2e73c7f4d6e20be9035e7be16a1
smart-life-tech <christlightworld@gmail.com>
2024-12-20 08:44:55 +0100

    new file:   camTests.py

---
+++ camTests.py
@@ -0,0 +1,26 @@
+from picamera2 import Picamera2
+import time
+
+# Initialize the camera
+camera = Picamera2()
+
+# Configure the camera with default settings
+camera.configure(camera.create_still_configuration(main={"size": camera.sensor_resolution}))
+
+# Start the camera
+camera.start()
+
+# Allow some time for the camera to initialize
+time.sleep(2)
+count = 0
+while True:
+    # Capture and save the image
+    camera.capture_file(f'high_res_image{count}.jpg')
+    count += 1
+    print("Image captured!")
+    time.sleep(1)  # Wait for 1 second before capturing the next image
+    # Capture and save the image
+    
+
+# Stop the camera
+camera.stop()
