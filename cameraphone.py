e97546680a8e6325cb6770fc8ed93069e4841d25
smart-life-tech <christlightworld@gmail.com>
2024-12-21 08:57:48 +0100

    modified:   cameraphone.py

--- cameraphone.py
+++ cameraphone.py
@@ -1,21 +1,20 @@
 import time
-from picamera import PiCamera
+from picamera2 import Picamera2
 import socket
 
 # Setup the camera
-camera = PiCamera()
-camera.resolution = (2048, 2048)
-camera.framerate = 15
-
+camera = Picamera2()
+# Configure the camera with default settings 
+camera.configure(camera.create_still_configuration(main={"size": camera.sensor_resolution}))
+# Start the camera 
+camera.start()
 # Define the server (Pi3rd) IP and port for communication
 PI3_IP = '192.168.0.3'  # Pi3's fixed IP address
 PORT = 5000
 
 def capture_image(filename):
-    camera.start_preview()
-    time.sleep(2)  # Allow the camera to adjust exposure
-    camera.capture(filename)
-    camera.stop_preview()
+    # Capture and save the image 
+    camera.capture_file(filename)
 
 def send_image_to_pi3(image_path):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -30,7 +29,7 @@ def wait_for_trigger():
         while True:
             data = s.recv(1024)
             if data.decode('utf-8') == 'capture':
-                filename = f"/home/pi/captured_image_{int(time.time())}.jpg"
+                filename = f"/home/user/camera/captured_image_{int(time.time())}.jpg"
                 capture_image(filename)
                 send_image_to_pi3(filename)
                 break
