7b7a42d1cbe921d97c73c19b4d541a125d6fbd56
smart-life-tech <christlightworld@gmail.com>
2025-05-09 17:27:17 +0100

    modified:   picServing2.py

--- picServing2.py
+++ picServing2.py
@@ -8,14 +8,13 @@ import socket
 import subprocess
 import requests
 from PIL import Image
-import os
+
 from picamera2 import Picamera2
 import RPi.GPIO as GPIO
-if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-    # Setup the camera
-    TRIGGER_PIN = 27  
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(TRIGGER_PIN, GPIO.OUT)
+# Setup the camera
+TRIGGER_PIN = 27  
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(TRIGGER_PIN, GPIO.OUT)
 # Get the local IP address of the Raspberry Pi
 hostname = socket.gethostname()
 current_ip = socket.gethostbyname(hostname)
